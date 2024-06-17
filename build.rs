8773b3c52cd6b24b41679cab8874f19d69ee2018
mt-johan <172122831+mt-johan@users.noreply.github.com>
2024-06-17 14:43:54 +0200

    Prepare for automated release workflow.

--- build.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    println!(
-        "cargo:rustc-env=CARGO_PKG_VERSION={}",
-        std::env::var("CARGO_PKG_VERSION_OVERRIDE").unwrap_or(String::from("0.0.0"))
-    );
-    println!("cargo:rerun-if-env-changed=CARGO_PKG_VERSION_OVERRIDE");
-}
